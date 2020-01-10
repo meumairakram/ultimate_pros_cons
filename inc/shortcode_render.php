8f17967e71ee2d80125236b67b81a3f9ca35a0c0
Umair Akram <contactumairakram@gmail.com>
2020-01-10 21:40:57 +0500

    render bug fix

--- inc/shortcode_render.php
+++ inc/shortcode_render.php
@@ -41,3 +41,93 @@ function upct_list_item_code($atts,$content) {
 }
 
 add_shortcode( 'upct_list_item', 'upct_list_item_code' );
+
+
+
+function upct_product_showcase_block($atts,$content) {
+
+    if(is_admin()) {
+        return false;
+    }
+
+    $atts = shortcode_atts(array(
+                            'product-image' => 'https://via.placeholder.net/300',
+                            'product-title' => '',
+                            'amazon-link' => '#',
+    ),$atts);
+
+    $prod_content = do_shortcode($content);
+
+    $image_link = $atts['product-image'];
+    $prod_title = $atts['product-title'];
+    $amazon_link = $atts['amazon-link'];
+            $output = <<<EOT
+            <div class="ua-clearfix"></div>
+            <div class="ua-product-showcase main-wrap">
+
+            <div class="ua-ps-inner">
+
+                <div class="ua-ps-img-holder">
+                <a href="$amazon_link" target="_blank"><img src="$image_link" alt=""></a>
+                </div>
+
+                <div class="ua-ps-content-holder">
+                <a href="$amazon_link" target="_blank"><span>$prod_title</span></a>
+                    
+                    <div class="features-wrap">
+                        <ul>
+                        $prod_content
+                        </ul>                
+                    </div>
+
+                    <div class="ua-ps-cta-wrap">
+                        <a href="$amazon_link" target="_blank">Check Price on Amazon</a>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+
+
+
+        <div class="ua-clearfix"></div>
+EOT;
+
+        return $output;
+}
+
+
+add_shortcode('upct_showcase','upct_product_showcase_block');
+
+
+
+function upct_create_buy_now_button($atts,$content) {
+
+    if(is_admin()) {
+        return false;
+    }
+
+    $atts = shortcode_atts(array(
+                            'product-link' => '#'
+                        ),$atts);
+
+    
+    $product_link = $atts['product-link'];
+    
+        $output =  <<<EOT
+        <div class="ua-cleafix"></div>
+        <div class="ua-cta-button-wrap">
+            <a href="$product_link" target="_blank">
+                            $content
+            </a>                
+        </div>
+        <div class="ua-cleafix"></div>
+EOT;
+
+    return $output;
+
+}
+
+add_shortcode('upct_buynow_button','upct_create_buy_now_button');
